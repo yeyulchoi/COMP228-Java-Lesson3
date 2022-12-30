4bcfb857ef5362ca5a65f38abb261c31ecbf4591
yeyulchoi <121686064+yeyulchoi@users.noreply.github.com>
2022-12-30 12:33:16 -0500

    tansfer files

---
+++ src/comp228_Java_lesson3/Program.java
@@ -0,0 +1,10 @@
+package comp228_Java_lesson3;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
